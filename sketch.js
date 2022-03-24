ff63c7dcfb38e67d2989416e676d4db30bb772ab
rachanne <ratoliao@gmail.com>
2022-03-23 22:36:31 -0700

    SOMETHING NEW

--- sketch.js
+++ sketch.js
@@ -1,44 +1,49 @@
 // different heart beats
 
-let heart = [{
-  name: "Dramatic",
-  color: "navy blue"
-}, {
-  name: "Stopping",
-  color: "black"
-}, {
-  name: "Regular",
-  color: "yellow"
-}, {
-  name: "Strong",
-  color: "purple"
-}, {
-  name: "Fast",
-  color: "red"
-}, {
-  name: "Hospital",
-  color: "white"
-}];
+let heart = ["achivement unlocked: you experienced passion",
+
+
+  "acheivement unlocked: you just died, RIP",
+
+
+  "acheivement unlocked: congratulations! you have been born!",
+
+
+  "acheivement unlocked: Love",
+
+
+  "acheivement unlocked: aw you got scared!",
+
+
+  "acheivement unlocked: you experience a love one die",
+];
+
+
 
 let randomIndex;
 let animating = false;
 let t = "Let's Play a Game of Life"
 let drawings = [];
 let imageCounter = 0;
-let mySound2;
-var myBeat = document.getElementById("myBeat")
-var hearts = document.getElementById("theHearts")
+let myImg
+
+let myBeat;
+let myBeat2;
+let myBeat3;
+let myBeat4;
+let myBeat5;
+let myBeat6;
 
 function preload() {
   for (let i = 0; i <= 5; i++) {
     drawings[i] = loadImage("assets/heart_" + i + ".JPG")
 
-    myImg0 = loadImage("assets/heart_0.jpg")
-    myImg1 = loadImage("assets/heart_1.jpg")
-    myImg2 = loadImage("assets/heart_2.jpg")
-    myImg3 = loadImage("assets/heart_3.jpg")
-    myImg4 = loadImage("assets/heart_4.jpg")
-    myImg5 = loadImage("assets/heart_5.jpg")
+    //  myImg = loadImage("assets/heart_0.JPG")
+    //  myImg1 = loadImage("assets/heart_1.JPG")
+    //  myImg2 = loadImage("assets/heart_2.JPG")
+    //  myImg3 = loadImage("assets/heart_3.JPG")
+    //  myImg4 = loadImage("assets/heart_4.JPG")
+    //  myImg5 = loadImage("assets/heart_5.JPG")
 
     myBeat = loadSound("sounds/sound_1.mp3")
     myBeat2 = loadSound("sounds/sound_2.mp3")
@@ -50,6 +55,8 @@ function preload() {
   }
 }
 
+
+
 function setup() {
   createCanvas(800, 600);
   background(220);
@@ -57,14 +64,14 @@ function setup() {
   imageMode(CENTER);
   frameRate(10);
 
-  text("click to start game", 50, 50);
+  text("Let's Play a Game of Life", 50, 50);
 
 }
 
 function draw() {
 
   if (animating == true) {
-    clear();
+
     image(drawings[imageCounter], 400, 400);
     imageCounter++;
 
@@ -83,45 +90,42 @@ function mousePressed() {
 function randomizer() {
 
   animating = false;
-  if (heart[0]) {
 
-    clear();
-    randomIndex = int(random(heart.length));
-    image(random(drawings), 400, 400);
 
-    text(heart[randomIndex].name, 300, 100);
+  randomIndex = int(random(heart.length));
+  image(random(drawings), 400, 400);
 
-  } else if (randomIndex == 0) {
-    image(myImg, 400, 400)
-    text('achivement unlocked: you experienced passion');
-    myBeat.play();
-
-  } else if (randomIndex == 1) {
-    image(myImg1, 400, 400)
-    text('acheivement unlocked: you just died, RIP');
-    myBeat2.play();
-
-  } else if (randomIndex == 2) {
-    image(myImg2, 400, 400)
-    text('acheivement unlocked: congratulations! you have been born!');
-    myBeat3.play();
-
-  } else if (randomIndex == 3) {
-    image(myImg3, 400, 400)
-    text('acheivement unlocked: Love');
-    myBeat4.play();
-
-  } else if (randomIndex == 4) {
-    image(myImg4, 400, 400)
-    text('acheivement unlocked: aw, you got scared!');
-    myBeat5.play();
-
-  } else if (randomIndex == 5) {
-    image(myImg5, 400, 400)
-    text('acheivement unlocked: you experience a love one die.');
-    myBeat6.play();
+  text(heart[randomIndex].name, 300, 100);
 
 }
+if (randomIndex == 0) {
+  image(myImg, 400, 400);
+  text('achivement unlocked: you experienced passion');
+  myBeat.play();
 
+} else if (randomIndex == 1) {
+  image(myImg1, 400, 400);
+  text('acheivement unlocked: you just died, RIP');
+  myBeat2.play();
+
+} else if (randomIndex == 2) {
+  image(myImg2, 400, 400);
+  text('acheivement unlocked: congratulations! you have been born!');
+  myBeat3.play();
+
+} else if (randomIndex == 3) {
+  image(myImg3, 400, 400);
+  text('acheivement unlocked: Love');
+  myBeat4.play();
+
+} else if (randomIndex == 4) {
+  image(myImg4, 400, 400);
+  text('acheivement unlocked: aw, you got scared!');
+  myBeat5.play();
+
+} else if (randomIndex == 5) {
+  image(myImg5, 400, 400);
+  text('acheivement unlocked: you experience a love one die.');
+  myBeat6.play();
 
 }
